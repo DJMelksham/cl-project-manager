3331ec80ed33c8cbedc4ddcc5d37e8d1e59661c4
Damien John Melksham <Damien.Melksham@gmail.com>
2016-02-23 22:45:05 +1100

    Few little changes here and there, mainly just checking on git-management.lisp

--- load-project.lisp
+++ load-project.lisp
@@ -7,6 +7,7 @@
 
 (load (merge-pathnames "document-functions.lisp" *load-truename*))
 (load (merge-pathnames "file-management.lisp" *load-truename*))
+(load (merge-pathnames "git-management.lisp" *load-truename*))
 (load (merge-pathnames "project-manager.lisp" *load-truename*))
 
 
