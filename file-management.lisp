60e47a867be35e575079a4dfb628f346429bf29b
Damien John Melksham <Damien.Melksham@gmail.com>
2016-03-01 08:58:38 +1100

    non-changes to file-management.lisp

--- file-management.lisp
+++ file-management.lisp
@@ -1,7 +1,8 @@
 (defun get-subfolders (path &optional (inclusive nil))
-  (let ((result (if inclusive (list (pathname path) nil)))
+  (let ((result (if inclusive (list (cl-fad:pathname-as-directory path) nil)))
 	(dirs (remove-if-not #'cl-fad:directory-pathname-p
-			     (cl-fad:list-directory (cl-fad:pathname-as-directory path)))))
+			     (cl-fad:list-directory (cl-fad:pathname-as-directory 
+						     (truename (pathname path)))))))
     (loop for dir in dirs
 	do (push dir result))
     (loop for dir in dirs
